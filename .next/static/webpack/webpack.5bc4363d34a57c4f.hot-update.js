150320bfb9138b44491d831ec83557ae5784dbb1
HenrySCCP <54988925+HenrySCCP@users.noreply.github.com>
2022-07-26 23:43:35 -0300

    Melhorias

--- .next/static/webpack/webpack.5bc4363d34a57c4f.hot-update.js
+++ .next/static/webpack/webpack.5bc4363d34a57c4f.hot-update.js
@@ -11,7 +11,7 @@ self["webpackHotUpdate_N_E"]("webpack",{},
 /******/ function(__webpack_require__) { // webpackRuntimeModules
 /******/ /* webpack/runtime/getFullHash */
 /******/ !function() {
-/******/ 	__webpack_require__.h = function() { return "38977940d3f0e931"; }
+/******/ 	__webpack_require__.h = function() { return "5d28fc0e0f859834"; }
 /******/ }();
 /******/ 
 /******/ /* webpack/runtime/compat */
--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,13 @@
+import Head from 'next/head';
 import '../styles/lista.scss';
 
 export default function MyApp({ Component, pageProps}){
     return (
         <>
+            <Head>
+                <title>Oceano | Teste de Front-End</title>
+                <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+            </Head>
             <Component {...pageProps} />
         </>
     )
